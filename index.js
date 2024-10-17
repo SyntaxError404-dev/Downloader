d44f193f9205944e979bc3e4cce9dd6619394e2f
SyntaxError404-dev <yoyoyonehal@gmail.com>
2024-10-17 14:49:31 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,28 +6,37 @@ const port = 3000;
 
 app.use(cors());
 
-app.get('/pin', async (req, res) => {
+app.get('/lyrics', async (req, res) => {
   try {
-    const { title, search } = req.query;
+    const { songName } = req.query;
 
-    if (!title || !search) {
-      return res.status(400).json({ error: 'Both title and search query are required' });
+    if (!songName) {
+      return res.status(400).json({ error: 'Song name is required' });
     }
 
-    const response = await axios({
-      method: 'get',
-      url: `https://smfahim.xyz/pin?title=${encodeURIComponent(title)}&search=${encodeURIComponent(search)}`,
-      responseType: 'json'
-    });
+    const apiResponse = await axios.get(`https://lyrist.vercel.app/api/${encodeURIComponent(songName)}`);
+    const data = apiResponse.data;
 
-    res.json(response.data);
+    if (data.lyrics) {
+      const title = data.title;
+      const artist = data.artist;
+      const lyrics = data.lyrics;
+
+      return res.status(200).json({
+        title,
+        artist,
+        lyrics
+      });
+    } else {
+      return res.status(404).json({ error: 'Lyrics not found' });
+    }
 
   } catch (error) {
-    console.error('Error fetching Pinterest images:', error);
-    res.status(500).json({ error: 'Error fetching Pinterest images' });
+    console.error('Error fetching lyrics:', error);
+    return res.status(500).json({ error: 'Internal server error' });
   }
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
